710f8d71ba26b821fd878d705e9e9f03467647d5
Arsham Shirvani <arshamshirvani@gmail.com>
2017-10-08 12:01:25 +0100

    set up project skeleton add license and readme files

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2017 Arsham Shirvani <arshamshirvani@gmail.com>. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 license
+// License that can be found in the LICENSE file.
+
+// Package Logpipe can redirect your application's `logs` to a generic logfile,
+// or to ElasticSearch for aggregate and view with kibana.
+package main
